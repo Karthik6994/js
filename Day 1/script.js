ed5decdfc3937b9d1eefa6845d1cabccf8cae723
Karthik <159438219+Karthik6994@users.noreply.github.com>
2025-06-24 20:02:57 +0530

    Add files via upload

---
+++ Day 1/script.js
@@ -0,0 +1,7 @@
+console.log("External Scripting");
+
+console.log("Hello World");            
+console.info("Hello World");           
+console.warn("Hello World");           
+console.error("Hello World");          
+console.debug("Hello World");
