333d6604cbbe01170c2c003c01b128bd135e17d6
Karthik <159438219+Karthik6994@users.noreply.github.com>
2025-06-24 20:04:25 +0530

    Update script.js

--- Day 1/script.js
+++ Day 1/script.js
@@ -1,7 +1,1 @@
 console.log("External Scripting");
-
-console.log("Hello World");            
-console.info("Hello World");           
-console.warn("Hello World");           
-console.error("Hello World");          
-console.debug("Hello World");
