384f8e5875ebe9d30397cafbf6f11c29f42375cb
Karthik <159438219+Karthik6994@users.noreply.github.com>
2025-07-10 20:17:58 +0530

    Add files via upload

---
+++ Day 2/script.js
@@ -0,0 +1,35 @@
+// Creating Personal Information Variables
+let name = "Karthik";
+let age = 24;
+let city = "Hyderabad";
+let country = "India";
+let email = "123@example.com";
+let gender = "Male";
+
+// Printing the personal info to the console
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("City:", city);
+console.log("Country:", country);
+console.log("Email:", email);
+
+//Craeted and showing different variables in the webpage
+document.write("Name:", name);
+document.write("\n Age:", age);
+document.write("\n City:", city);
+document.write("\n Country:", country);
+document.write("\n Email:", email);
+
+//Short explaination of variable
+document.write("  Variable: It is a named location/memory location which is used to store the data/value/literal for temporary purpose")
+console.log("Variable: It is a named location/memory location which is used to store the data/value/literal for temporary purpose")
+
+//Creating table using console
+console.table({
+    Name:"Karthik",
+    Age:24,
+    Batch:"39R",
+    City:"Hyderabad"
+})
+
+
